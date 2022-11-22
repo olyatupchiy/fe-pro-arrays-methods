c653521fb036e1e291f32343b543ce665f3ead2f
olyatupchiy <olya.tupchiy@gmail.com>
2022-11-22 12:23:52 +0100

    Finished

--- src/index.js
+++ src/index.js
@@ -4,56 +4,56 @@ import { goods } from './goods.js';
  * @param {string} brand
  * @returns {*}
  */
-const brandFilter = (brand) => {};
+const brandFilter = (brand) => goods.filter(item => brand === item.brand);
 
-/**
+  /**
  * @param {string} color
  * @returns {*}
  */
-const colorFilter = (color) => {};
+const colorFilter = (color) => goods.filter(item => color === item.color);
 
 /**
  * @param {string} model
  * @returns {*}
  */
-const modelFilter = (model) => {};
+const modelFilter = (model) => goods.filter(item => model === item.model);
 
 /**
  * @param {number} memory
  * @returns {*}
  */
-const memoryFilter = (memory) => {};
+const memoryFilter = (memory) => goods.filter(item => memory === item.memory);
 
 /**
  * @param {number} price
  * @returns {*}
  */
-const priceFilter = (price) => {};
+const priceFilter = (price) => goods.filter(item => price === item.price);
 
 /**
  * @param {string} country
  * @returns {*}
  */
-const countryFilter = (country) => {};
+const countryFilter = (country) => goods.filter(item => country === item.country);
 
 /**
  * @param {string} os
  * @returns {*}
  */
-const osFilter = (os) => {};
+const osFilter = (os) => goods.filter(item => os === item.os);
 
 /**
  * @param {number} from
  * @param {number} to
  */
-const rangeFilter = (from, to) => {};
+const rangeFilter = (from, to) => goods.filter(item => from <= item.price && item.price <= to);
 
-const minPriceReducer = () => {};
+const minPriceReducer = () => goods.reduce((acum, item) => (item.price < acum) ? item.price : acum, Infinity);
 
-const maxPriceReducer = () => {};
+const maxPriceReducer = () => goods.reduce((acum, item) => (item.price > acum) ? item.price : acum, 0);
 
-const toMaxSorter = () => {};
-const toMinSorter = () => {};
+const toMaxSorter = () => goods.sort((min, max) => max.price - min.price);
+const toMinSorter = () => goods.sort((min, max) => min.price - max.price);
 
 export const filters = {
   brandFilter,
